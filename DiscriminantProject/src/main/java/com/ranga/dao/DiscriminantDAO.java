67b716914c5a2f0f535c89e211057e29a9450240
velovan <velovan@users.noreply.github.com>
2021-11-14 12:00:19 +0200

    Update DiscriminantDAO.java

--- DiscriminantProject/src/main/java/com/ranga/dao/DiscriminantDAO.java
+++ DiscriminantProject/src/main/java/com/ranga/dao/DiscriminantDAO.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.ranga.model.Discriminant;
  
 public interface DiscriminantDAO {
+ 
     public long createDiscriminant(Discriminant discriminant);
     public Discriminant updateDiscriminant(Discriminant discriminant);
     public void deleteDiscriminant(long id);
     public List<Discriminant> getAllDiscriminant();
     public Discriminant getDiscriminant(long id);	
     public List<Discriminant> getAllDiscriminant(float discriminantName); 
+ 
 }
