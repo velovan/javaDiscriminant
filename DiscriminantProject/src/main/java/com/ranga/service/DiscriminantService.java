b2db9fc5abd34e13fd1307224cb7f1555766c98a
velovan <velovan@users.noreply.github.com>
2021-11-21 15:30:22 +0200

    Update DiscriminantService.java

--- DiscriminantProject/src/main/java/com/ranga/service/DiscriminantService.java
+++ DiscriminantProject/src/main/java/com/ranga/service/DiscriminantService.java
@@ -5,7 +5,8 @@ import com.ranga.model.Discriminant;
 
  
 public interface DiscriminantService { 
-	public long createDiscriminantt(Discriminant discriminantt);
+	
+    public long createDiscriminantt(Discriminant discriminantt);
     public Discriminant updateDiscriminantt(Discriminant discriminantt);
     public void deleteDiscriminantt(long id);
     public List<Discriminant> getAllDiscriminant();
